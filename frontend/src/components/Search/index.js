0d52433ebd9e5c2013ca3bf9d966441f77498227
Vincentfarcy <v.farcy72@gmail.com>
2022-02-02 22:19:14 +0100

    first step of search

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -25,7 +25,7 @@ const Search = ({
 
   const classes = useStyles();
   
-  const searchSteps = ['resource', 'field', 'results'];
+  const searchSteps = ['start', 'field', 'results'];
   const [searchStep, setSearchStep] = useState(0);
   const [selectedResource, setSelectedResource] = useState();
   const [searchFields, setSearchFields] = useState([]);
@@ -38,8 +38,8 @@ const Search = ({
     selectedFieldValues = fieldValues[selectedField.name];
   }
   
-  console.log('>> searchStep:', searchStep);
   console.log('>> selectedResource:', selectedResource);
+  console.log('>> searchStep:', searchStep);
   console.log('>> searchFields:', searchFields);
   console.log('>> selectedField:', selectedField);
   console.log('>> selectedFieldValues:', selectedFieldValues);
@@ -53,8 +53,7 @@ const Search = ({
   }
   
   const handleNewSearchClick = () => {
-    setSearchStep(getSearchStep('resource'));
-    setSelectedResource(null);
+    setSearchStep(getSearchStep('start'));
     setSearchFields([]);
     setSelectedField(null);
     setSelectedValues([]);
@@ -64,23 +63,10 @@ const Search = ({
   const goToNextField = (resource, field, backward=false) => {
     const nextField = findNextField(resource, field, backward);
     if (nextField) {
-      handleFieldClick(resource, nextField);
+      handleFieldClick(nextField);
       return nextField;
     }
   }
-
-  const handleResourceClick = (resource) => {
-    setResults(null);
-    if (resource !== selectedResource) {
-      setSelectedResource(resource);
-      setSearchFields(searchConfig.find( resourceConfig => resourceConfig === resource ).fields);
-      const nextField = goToNextField(resource, null);
-      if (! nextField) {
-        setSelectedField();
-      }
-    }
-    setSelectedValues([]);
-  };
   
   const findNextField = (resource, selectedField, backward=false) => {
     if (! resource) {
@@ -113,8 +99,16 @@ const Search = ({
   const handleClickRightCriteriaChevron = () => {
     goToNextField(selectedResource, selectedField);
   }
+  
+  const setChosenField = (field, value, replace=false) => {
+    // no field to add if default choice
+    if (value.choice !== 'default') {
+      searchFields.splice(searchFields.indexOf(field)+1 , replace?1:0, value)
+      setSearchFields(searchFields);
+    }
+  }
 
-  const handleFieldClick = (resource, field) => {
+  const handleFieldClick = (field) => {
     setSearchStep(getSearchStep('field'));
     if (field !== selectedField) {
       setSelectedField(field);
@@ -123,6 +117,9 @@ const Search = ({
   
   const handleValueClick = (field, value) => {
     if (value) {
+      if (! value.id) {
+        value.id = value.name
+      }
       const currentValueForField = selectedValues.find(selectedValue => selectedValue.field === field);
       if (! currentValueForField) {
         selectedValues.push({
@@ -130,11 +127,17 @@ const Search = ({
           value: value
         })
         setSelectedValues([...selectedValues]);
+        if (field.type === 'choice') {
+          setChosenField(field, value)
+        }
       } else {
         if (currentValueForField.value.id === value.id) {
           goToNextField(selectedResource, field);
           return;
         } else {
+          if (field.type === 'choice') {
+            setChosenField(field, value, true)
+          }
           setSelectedValues(selectedValues.map(selectedValue => {
             if (selectedValue.field === field) {
               selectedValue.value = value
@@ -157,19 +160,21 @@ const Search = ({
   }
   
   const getResults = async () => {
-    if (!selectedResource || !resourceValues) {
+    if (!resourceValues) {
       return;
     }
     let results = resourceValues.data;
     console.log('--1', results);   
     selectedValues.forEach(selectedValue => {
-      results = results.filter(result => {
-        // single value to array :
-        let resultValues = [].concat(result[selectedValue.field.name])
-        console.log('--2', selectedValue.value.id);
-        console.log('--3', resultValues);
-        return resultValues.includes(selectedValue.value.id);
-      })
+      if (selectedValue.field.type !== 'choice') {
+        results = results.filter(result => {
+          // single value to array :
+          let resultValues = [].concat(result[selectedValue.field.name])
+          console.log('--2', selectedValue.value.id);
+          console.log('--3', resultValues);
+          return resultValues.includes(selectedValue.value.id);
+        })
+      }
     })
     console.log('--9', results);   
     setResults(results);
@@ -193,23 +198,32 @@ const Search = ({
   }
 
   //== Get all data
-  useEffect( () => { 
-    const rootContainer = searchConfig[0];
-    fetchContainer(rootContainer);
-    rootContainer.fields.forEach(field => {
-      if (field.type === 'boolean') {
+  const getData = (data) => {
+    data.forEach(field => {
+      switch (field.type) {
+        case 'choice':
+        getData(field.fields);
+        break;
+        case 'boolean':
         addBooleanField(field);
-      } else if (field) {
+        break;
+        default:
         fetchContainer(field);
       }
     })
+  }
+  
+  useEffect( () => { 
+    const rootContainer = searchConfig[0];
+    fetchContainer(rootContainer);
+    getData(rootContainer.fields);
+    setSelectedResource(rootContainer);
+    setSearchFields(rootContainer.fields);
+    goToNextField(rootContainer, null);
   }, []);
   
   return (
     <Container className={classes.mainContainer} maxWidth="lg">
-      { ! selectedResource &&
-        <h1>Que recherchez-vous ?</h1>
-      }
       { selectedResource &&
         <>
           <h1>Rechercher {selectedResource.label}</h1>
@@ -221,7 +235,7 @@ const Search = ({
                     <Button 
                       variant="contained" 
                       disabled={selectedField === field}
-                      onClick={()=>handleFieldClick(selectedResource, field)}
+                      onClick={()=>handleFieldClick(field)}
                     >
                       {field.label}
                     </Button>
@@ -266,19 +280,21 @@ const Search = ({
             <Box pb={1} mt={-1} className={classes.selectedCriterias}>
               {
                 selectedValues.map((selectedValue, index) => {
-                  const label = selectedValue.field.type!=='boolean'
-                    ? selectedValue.value.label
-                    : selectedValue.field.label + ' : ' + selectedValue.value.label
-                  return (
-                  selectedValue &&
-                    <Box pt={1} pl={2} key={index}>
-                      <Chip 
-                        label={label}
-                        onClick={()=>handleDeleteSelectedValueClick(selectedValue.field, selectedValue.value)}
-                        onDelete={()=>handleDeleteSelectedValueClick(selectedValue.field, selectedValue.value)}
-                      />
-                    </Box>
-                  )
+                  if (selectedValue.field.type !== 'choice') {
+                    const label = selectedValue.field.type!=='boolean'
+                      ? selectedValue.value.label
+                      : selectedValue.field.label + ' : ' + selectedValue.value.label
+                    return (
+                    selectedValue &&
+                      <Box pt={1} pl={2} key={index}>
+                        <Chip 
+                          label={label}
+                          onClick={()=>handleDeleteSelectedValueClick(selectedValue.field, selectedValue.value)}
+                          onDelete={()=>handleDeleteSelectedValueClick(selectedValue.field, selectedValue.value)}
+                        />
+                      </Box>
+                    )
+                  }
                 })
               }
             </Box>
@@ -287,13 +303,13 @@ const Search = ({
       }
       { searchStep !== getSearchStep('results') &&
         <>
-          { searchStep !== getSearchStep('resource') &&
+          { searchStep !== getSearchStep('start') &&
             <h2>Précisez votre recherche :</h2>
           }
           <Box pb={4} mt={-1} className={classes.criteriasContainer}>
             { selectedResource &&
               <Box className={classes.criteriaChevronContainer}>
-                { searchStep !== getSearchStep('resource') &&
+                { searchStep !== getSearchStep('start') &&
                   <ChevronLeftIcon
                     className={classes.criteriaChevron}
                     onClick={() => handleClickLeftCriteriaChevron()}
@@ -301,54 +317,57 @@ const Search = ({
                 }
               </Box>
             }
-            { selectedField === null &&
-              <Box className={classes.criteriaContainer}>              
-                {
-                  searchConfig.map((resource, index) => (
-                    <Box pt={2} key={index} className={classes.resourceContainer}>
+            { 
+              searchFields.filter(field => selectedField === field).map((field, index) => {
+                if (field.type !== 'choice') { return(
+                  <Box key={index} className={classes.criteriaContainer}>
+                    <Box className={ (selectedFieldValues?.length > 6) ? classes.manyCriterias : null }>
+                      {
+                        selectedFieldValues?.map((value, index) => (
+                          <Box pt={2} key={index}>
+                            <Button 
+                              variant="contained" 
+                              color={selectedValues.find(selectedValue => (selectedValue.value.id === value.id)) ? "primary" : "secondary"}
+                              onClick={()=>handleValueClick(field, value)}
+                            >
+                              {value.label}
+                            </Button>
+                          </Box>
+                        ))
+                      }
+                    </Box>
+                    <Box pt={3}>
                       <Button 
                         variant="contained" 
-                        color={selectedResource === resource ? "primary" : "secondary"}
-                        onClick={()=>handleResourceClick(resource)}
-                        /*startIcon={<ResourceIcon resource={resource.name} />}*/
+                        color="default"
+                        className={classes.noChoiceButton}
+                        onClick={()=>handleValueClick(field, null)}
                       >
-                        {resource.label}
+                        Ignorer ce critère
                       </Button>
                     </Box>
-                  ))
-                }
-              </Box>
-            }
-            { 
-              searchFields.filter(field => selectedField === field).map((field, index) => (
-                <Box key={index} className={classes.criteriaContainer}>
-                  <Box className={ (selectedFieldValues?.length > 6) ? classes.manyCriterias : null }>
-                    {
-                      selectedFieldValues?.map((value, index) => (
-                        <Box pt={2} key={index}>
-                          <Button 
-                            variant="contained" 
-                            color={selectedValues.find(selectedValue => (selectedValue.value.id === value.id)) ? "primary" : "secondary"}
-                            onClick={()=>handleValueClick(field, value)}
-                          >
-                            {value.label}
-                          </Button>
-                        </Box>
-                      ))
-                    }
                   </Box>
-                  <Box pt={3}>
-                    <Button 
-                      variant="contained" 
-                      color="default"
-                      className={classes.noChoiceButton}
-                      onClick={()=>handleValueClick(field, null)}
-                    >
-                      Ignorer ce critère
-                    </Button>
+                )} else { 
+                  return(
+                  <Box key={index} className={classes.criteriaContainer}>
+                    <Box className={ (selectedFieldValues?.length > 6) ? classes.manyCriterias : null }>
+                      {
+                        selectedField.fields.map((value, index) => (
+                          <Box pt={2} key={index}>
+                            <Button 
+                              variant="contained" 
+                              color={selectedValues.find(selectedValue => (selectedValue.value.id === value.id)) ? "primary" : "secondary"}
+                              onClick={()=>handleValueClick(field, value)}
+                            >
+                              {value.label}
+                            </Button>
+                          </Box>
+                        ))
+                      }
+                    </Box>
                   </Box>
-                </Box>
-              ))
+                )}
+              })
             }
             { selectedResource &&
               <Box className={classes.criteriaChevronContainer}>
@@ -377,7 +396,7 @@ const Search = ({
                 }
                 <List sx={{/* width: '100%', maxWidth: 360, bgcolor: 'background.paper' */}}>
                   { results.map((result, index) => (
-                    <ListItem>
+                    <ListItem key={index}>
                       <ListItemAvatar>
                         <Avatar>
                           <WorkIcon />
@@ -394,7 +413,7 @@ const Search = ({
             }
           </Box>
         }
-        { searchStep !== getSearchStep('resource') &&
+        { searchStep !== getSearchStep('start') &&
           <Box pt={4}>
             <hr />
             <Box pt={3}>
