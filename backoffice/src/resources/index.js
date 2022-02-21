8c00f1002dec3480f292308d33d9a0672ec1d7c4
Vincentfarcy <v.farcy72@gmail.com>
2022-02-21 21:24:39 +0100

    backoffice menu

--- backoffice/src/resources/index.js
+++ backoffice/src/resources/index.js
@@ -1,14 +1,15 @@
+
 export { default as Organization } from "./Organization";
-export { default as FAQ } from "./FAQ";
-export { default as Person } from "./Person";
 export { default as Program } from "./Program";
 export { default as Configuration } from "./Configuration";
+export { default as FAQ } from "./FAQ";
+export { default as Person } from "./Person";
 
 // Concepts
 export { default as Concept } from './Concept';
-export { default as BusinessCreationGoal } from './Concept/BusinessCreationGoal';
-export { default as DegreeLevel } from './Concept/DegreeLevel';
 export { default as Gender } from './Concept/Gender';
-export { default as FindingHelpGoal } from './Concept/FindingHelpGoal';
+export { default as DegreeLevel } from './Concept/DegreeLevel';
+export { default as BusinessCreationGoal } from './Concept/BusinessCreationGoal';
 export { default as JobSearchGoal } from './Concept/JobSearchGoal';
+export { default as FindingHelpGoal } from './Concept/FindingHelpGoal';
 export { default as TrainingGoal } from './Concept/TrainingGoal';
